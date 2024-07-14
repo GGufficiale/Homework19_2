f48482d91f543f02dfd7bc3e6bf2daa2e9e3b98a
GGufficiale <germangranovskii@mail.ru>
2024-07-14 20:42:18 +0300

    Намутил два шаблона для домашней страницы и страницы с контактной информацией.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog.views import home
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', include('catalog.urls', namespace='catalog')),
+    path('', home, name='home')
 ]
