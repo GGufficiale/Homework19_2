0f1c4dc9ebbe044448e07bd2755c1faf15b14215
GGufficiale <germangranovskii@mail.ru>
2024-07-14 20:53:24 +0300

    Реализован сбор обратной связи от пользователя, который зашел на страницу контактов и отправил свои данные для обратной связи.

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def index(request):
+    """Функция принимает параметр request (инфа от пользователя на фротэнде) и возвращает ответ"""
+    if request.method == "POST":
+        """В request хранится информация о методе, который отправлял пользователь"""
+        name = request.POST.get('name')
+        """И передается информация, которую заполнил пользователь"""
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, 'main/index.html')
